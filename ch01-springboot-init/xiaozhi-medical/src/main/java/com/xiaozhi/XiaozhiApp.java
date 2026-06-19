47342996bc5e586dfe892c98e67db14eb953a253
# <#>
2026-06-19 23:43:21 +0800

    feat: ch01 - Spring Boot project init with Knife4j

---
+++ ch01-springboot-init/xiaozhi-medical/src/main/java/com/xiaozhi/XiaozhiApp.java
@@ -0,0 +1,13 @@
+package com.xiaozhi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XiaozhiApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XiaozhiApp.class, args);
+	}
+
+}
---
+++ ch01-springboot-init/xiaozhi-medical/src/test/java/com/xiaozhi/XiaozhiAppTests.java
@@ -0,0 +1,13 @@
+package com.xiaozhi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XiaozhiAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
